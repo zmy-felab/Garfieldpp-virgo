df228de4d5df1d0f7d16cd21c5546975e442ec35
Virgo825 <zhoujianjin825@gmail.com>
2020-08-14 09:07:58 +0800

    shell run

--- GainParameter/run.sh
+++ GainParameter/run.sh
@@ -7,19 +7,17 @@ if [ ! -d "./runlog" ];then
     mkdir runlog
 fi
 
-voltage=760
+nEvents=10
+voltage=700
 pressure=760.0
 temperature=293.15
 
-for i in `seq 1 5`
+for i in `seq 0 1`
 do
-    let voltage=voltage+20
-    let presure+=1
-    presure=`expr $presure + 1`
-    temperature=`echo "$temperature+0.5"|bc`
-    echo $i $voltage $presure $temperature
+    # let voltage=voltage+20
+    pressure=`echo "$pressure-1."|bc`
+    # temperature=`echo "$temperature+2."|bc`
+    echo $i $nEvents $voltage $pressure $temperature
+
+    ./CeramicGEM -n $nEvents -v $voltage -p pressure -t $temperature > "./runlog/${nEvents}_${voltage}_${pressure}_${temperature}.log" 
 done
-
-
-
-
